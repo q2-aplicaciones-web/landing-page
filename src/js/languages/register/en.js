070182887c13c7c121ed361714e74218a8f89139
JonatanFD <u20211b995@upc.edu.pe>
2025-04-24 08:49:01 -0500

    feat: add languages

---
+++ src/js/languages/register/en.js
@@ -0,0 +1,14 @@
+const en = {
+    "title_register": "Register | Quri",
+    "btn_back": "Go back to hub",
+    "header_register": "Register",
+    "label_first_name": "First Name:",
+    "label_second_name": "Second Name:",
+    "label_username": "Username:",
+    "label_email": "Email:",
+    "label_phone": "Phone Number:",
+    "label_accept_terms": "I accept",
+    "link_terms": "Terms and Conditions",
+    "label_offers": "I want to receive special offers, new drops, and early access to exclusive designs.",
+    "btn_register": "Register"
+}
---
+++ src/js/languages/register/es.js
@@ -0,0 +1,14 @@
+const es = {
+    "title_register": "Registrar | Quri",
+    "btn_back": "Volver al hub",
+    "header_register": "Registrar",
+    "label_first_name": "Primer Nombre:",
+    "label_second_name": "Segundo Nombre:",
+    "label_username": "Nombre de Usuario:",
+    "label_email": "Correo Electrónico:",
+    "label_phone": "Número de Teléfono:",
+    "label_accept_terms": "Acepto",
+    "link_terms": "Términos y Condiciones",
+    "label_offers": "Quiero recibir ofertas especiales, nuevos lanzamientos y acceso anticipado a diseños exclusivos.",
+    "btn_register": "Registrar"
+}
