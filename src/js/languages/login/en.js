f58f96a82f36de12024635f115f4845c27747a23
JonatanFD <u20211b995@upc.edu.pe>
2025-04-24 08:52:32 -0500

    feat: add languages to login

---
+++ src/js/languages/login/en.js
@@ -0,0 +1,10 @@
+const en = {
+    "title_login": "Login",
+    "btn_back": "Go back",
+    "header_login": "Login",
+    "label_email": "Username/Email:",
+    "label_password": "Password:",
+    "link_forgot_password": "Forgot password?",
+    "link_no_account": "Don’t have an account?",
+    "btn_login": "Login"
+}
---
+++ src/js/languages/login/es.js
@@ -0,0 +1,10 @@
+const es = {
+    "title_login": "Iniciar sesión",
+    "btn_back": "Volver",
+    "header_login": "Iniciar sesión",
+    "label_email": "Usuario/Correo electrónico:",
+    "label_password": "Contraseña:",
+    "link_forgot_password": "¿Olvidaste tu contraseña?",
+    "link_no_account": "¿No tienes una cuenta?",
+    "btn_login": "Iniciar sesión"
+}
